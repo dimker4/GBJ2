0eb7aedda502a9134672b3c7bc441bf1ba4e025d
dimker <dimker4@gmail.com>
2020-06-18 18:16:18 +0500

    #0

---
+++ src/GBJ2/J2/Lesson1/Lesson1.java
@@ -0,0 +1,4 @@
+package GBJ2.J2.Lesson1;
+
+public class Lesson1 {
+}
--- src/GBJ2/J2/Marathon/Team.java
+++ src/GBJ2/J2/Marathon/Team.java
@@ -1,18 +1,39 @@
 package GBJ2.J2.Marathon;
 
 public class Team {
-    String name;
-    Competitor[] competitors;
+    private String name;
+    private Competitor[] teammates;
 
-    Team (String name, Competitor ...arr) {
+    Team (String name, Competitor ...arr) { // Конструтор с множественными параметрами
         if (arr.length > 0 & !name.isEmpty()) {
             this.name = name;
-            competitors = new Competitor[arr.length];
-            for (int i = 0; i < competitors.length; i ++) {
-                competitors[i] = arr[i];
+            teammates = new Competitor[arr.length];
+            for (int i = 0; i < teammates.length; i ++) {
+                teammates[i] = arr[i];
             }
         }
     }
 
+    public void getTeamMembers () {
+        System.out.println("Состав команды " + this.name + ": ");
+        for (int i = 0; i < teammates.length; i ++) {
+            teammates[i].info();
+        }
+    }
+
+    public void getTeamSuccessfulMembers () {
+        System.out.println("Участники, прошедшие всю полосу препятствий: ");
+        for (int i = 0; i < teammates.length; i ++) {
+            if (teammates[i].isOnDistance()) teammates[i].info();
+        }
+    }
+
+    public Competitor[] getTeamList() { // Так как массив competitors приватный, то переопределим и вернем новый
+        Competitor[] arr = new Competitor[teammates.length];
+        for (int i = 0; i < teammates.length; i ++) {
+            arr[i] = teammates[i];
+        }
+        return arr;
+    }
 
 }
