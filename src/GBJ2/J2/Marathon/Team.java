4cb57cd4d18cc3a3f7503a5d865ae3a691fe77ec
dimker <dimker4@gmail.com>
2020-06-18 18:05:51 +0500

    Lesson1J2_1

--- src/GBJ2/J2/Marathon/Team.java
+++ src/GBJ2/J2/Marathon/Team.java
@@ -1,4 +1,18 @@
 package GBJ2.J2.Marathon;
 
 public class Team {
+    String name;
+    Competitor[] competitors;
+
+    Team (String name, Competitor ...arr) {
+        if (arr.length > 0 & !name.isEmpty()) {
+            this.name = name;
+            competitors = new Competitor[arr.length];
+            for (int i = 0; i < competitors.length; i ++) {
+                competitors[i] = arr[i];
+            }
+        }
+    }
+
+
 }
